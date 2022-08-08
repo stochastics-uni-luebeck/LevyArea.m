3744dfcc0c2459be106379ddb9dd42f8dce03b32
Felix Kastner <kastner.felix@gmail.com>
2022-08-08 16:51:35 +0200

    add Matlab Contents.m files

---
+++ +levyarea/Contents.m
@@ -0,0 +1,14 @@
+% levyarea package -- Part of LevyArea.m.
+% Version 1.0.0 20-Jan-2022
+%
+% Main routines
+%   iterated_integrals - Simulates twice iterated stochastic integrals.
+%   simulate           - The core routine (no special parameter handling).
+% Auxiliary routines
+%   optimal_algorithm  - Determines the optimal algorithm for iterated_integrals.
+% Levy area routines
+%   fourier            - Simulates Levy area using the Fourier algorithm.
+%   milstein           - Simulates Levy area using the Milstein algorithm.
+%   wiktorsson         - Simulates Levy area using the Wiktorsson algorithm.
+%   mronroe            - Simulates Levy area using the Mrongowius-Roessler algorithm.
+
